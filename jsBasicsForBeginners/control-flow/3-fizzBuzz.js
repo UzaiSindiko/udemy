b24b4b7aca15b1b413ac006536244c50b7cfc338
andreassosilo <andreas.sosilo@gmail.com>
2019-07-17 12:17:32 +0700

    Modify 3-fizzBuzz.js

--- jsBasicsForBeginners/control-flow/3-fizzBuzz.js
+++ jsBasicsForBeginners/control-flow/3-fizzBuzz.js
@@ -1,19 +1,18 @@
+// Exercise - FizzBuzz
+// JavaScript Basics for Beginners
 
-const output = fizzBuzz(17);
-console.log(output);
+const output = fizzBuzz(15)
+console.time('Performance')
+console.log(output)
+console.timeEnd('Performance')
 
-function fizzBuzz(input) {
-  if (typeof input !== 'number')
-    return NaN; 
-
-  if ((input % 3 === 0) && (input % 5 === 0))
-    return 'FizzBuzz'; 
-
-  if (input % 3 === 0)
-    return 'Fizz'; 
-
-    if (input % 5 === 0)
-    return 'Buzz'; 
-
-  return input; 
+function fizzBuzz (input) {
+  if (typeof input !== 'number') { return NaN }
+  // If the number is divisible by 3 & 5, we get 'FizzBuzz'
+  if ((input % 3 === 0) && (input % 5 === 0)) { return 'FizzBuzz' }
+  // If the number is divisible by 3, we get 'Fizz'
+  if (input % 3 === 0) { return 'Fizz' }
+  // If the number is divisible by 5, we get 'Buzz'
+  if (input % 5 === 0) { return 'Buzz' }
+  return input
 }
