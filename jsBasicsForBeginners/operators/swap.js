0c1622a0dbefb61e20df8ea218d75e4ed1577be7
andreassosilo <andreas.sosilo@gmail.com>
2019-07-17 11:50:30 +0700

    Modify swap.js exercise

---
+++ jsBasicsForBeginners/operators/swap.js
@@ -0,0 +1,13 @@
+// Exercise - Swapping Variables
+// JavaScript Basics for Beginners
+
+let a = 'red'
+let b = 'blue'
+
+// Answers to swap value in variables
+let temp = a
+a = b
+b = temp
+
+console.log(a)
+console.log(b)
