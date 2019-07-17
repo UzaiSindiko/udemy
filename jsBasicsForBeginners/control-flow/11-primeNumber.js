4f5a7a82a2af7d9b9e9682764ee9df3a68fa4c8c
andreassosilo <andreas.sosilo@gmail.com>
2019-07-17 17:32:25 +0700

    Modify 11-primeNumber.js

--- jsBasicsForBeginners/control-flow/11-primeNumber.js
+++ jsBasicsForBeginners/control-flow/11-primeNumber.js
@@ -1,15 +1,20 @@
+// Exercise - Prime Numbers
+// JavaScript Basics for Beginners
 
-showPrimes(10);
+console.time('Performance')
+showPrimes(10)
+console.timeEnd('Performance')
 
-function showPrimes(limit) {
-  for (let number = 2; number <= limit; number++) 
-    if (isPrime(number)) console.log(number); 
+function showPrimes (limit) {
+  for (let number = 2; number <= limit; number++) {
+    if (isPrime(number)) console.log(number)
+  }
 }
 
-function isPrime(number) {
-  for (let factor = 2; factor < number; factor++) 
-    if (number % factor === 0) 
-      return false; 
-  
-  return true; 
+function isPrime (number) {
+  for (let factor = 2; factor < number; factor++) {
+    if (number % factor === 0) { return false }
+  }
+
+  return true
 }
