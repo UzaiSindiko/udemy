ac7686ecebc7f2a1e878f695711820bc21ecdc39
andreassosilo <andreas.sosilo@gmail.com>
2019-07-17 15:55:40 +0700

    Modify 6-countTruthy.js

--- jsBasicsForBeginners/control-flow/6-countTruthy.js
+++ jsBasicsForBeginners/control-flow/6-countTruthy.js
@@ -1,12 +1,15 @@
+// Exercise - Count Truthy
+// JavaScript Basics for Beginners
 
-const array = [0, null, undefined, '', 2, 3];
+const array = [0, null, undefined, '', 2, 3]
+console.time('Performance')
+console.log(countTruthy(array))
+console.timeEnd('Performance')
 
-console.log(countTruthy(array));
-
-function countTruthy(array) {
-  let count = 0; 
-  for (let value of array) 
-    if (value)
-      count++;
-  return count; 
+function countTruthy (array) {
+  let count = 0
+  for (let value of array) {
+    if (value) { count++ }
+  }
+  return count
 }
