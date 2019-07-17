dacc4c7b44c0cccde5f0dcbdf59c5a0d741deab9
andreassosilo <andreas.sosilo@gmail.com>
2019-07-17 16:47:48 +0700

    Modify

--- jsBasicsForBeginners/control-flow/7-stringProperties.js
+++ jsBasicsForBeginners/control-flow/7-stringProperties.js
@@ -1,16 +1,19 @@
+// Exercise - String Properties
+// JavaScript Basics for Beginners
 
-const movie = { 
+const movie = {
   title: 'a',
   releaseYear: 2018,
   rating: 4.5,
   director: 'b'
-};
+}
 
-showProperties(movie);
+showProperties(movie)
 
-function showProperties(obj) {
+function showProperties (obj) {
   for (let key in obj) {
-    if (typeof obj[key] === 'string')
-      console.log(key, obj[key]);
+    if (typeof obj[key] === 'string') {
+      console.log(key, obj[key])
+    }
   }
 }
