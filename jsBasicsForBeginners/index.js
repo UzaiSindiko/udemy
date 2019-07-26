b3497a68fe37a809cccb8fc7b456c0e9f43356a8
andreassosilo <andreas.sosilo@gmail.com>
2019-07-26 22:32:48 +0700

    Modify index.js

--- jsBasicsForBeginners/index.js
+++ jsBasicsForBeginners/index.js
@@ -273,3 +273,17 @@ console.log('some() -> one element is true is enough : ' + atLeastOnePositive)
 numbers = [1, -1, 2, 3]
 const filtered = numbers.filter(function (value) { return value >= 0 })
 console.log('Filtered array: ' + filtered)
+
+// Mapping an array using map()
+let items = filtered.map(n => '<li>' + n + '</li>')
+const html = '<ul>' + items.join('') + '</ul>'
+console.log(html)
+
+// Chaining method of built-in function
+numbers = [1, -1, 2, 3]
+items = numbers
+  .filter(n => n >= 0)
+  .map(n => ({ value: n }))
+  .filter(obj => obj.value > 1)
+  .map(obj => obj.value)
+console.log(items)
