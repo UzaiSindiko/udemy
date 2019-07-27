7f0461cb583a45ea28093c9aff3b8d8a2158a3df
andreassosilo <andreas.sosilo@gmail.com>
2019-07-27 07:35:51 +0700

    Modify 4-move.js

--- jsBasicsForBeginners/arrays/4-move.js
+++ jsBasicsForBeginners/arrays/4-move.js
@@ -1,19 +1,22 @@
 
-const numbers = [1, 2, 3, 4]; 
+const numbers = [1, 2, 3, 4]
 
-const output = move(numbers, 1, 3);
+const output = move(numbers, 1, 2)
 
-console.log(output); 
+console.log(output)
 
-function move(array, index, offset) { 
-  const position = index + offset;  
+function move (array, index, offset) {
+  const position = index + offset
+  // Exception handling if the position is outside the array range
   if (position >= array.length || position < 0) {
-    console.error('Invalid offset.');
-    return; 
+    console.error('Invalid offset.')
+    return
   }
-  
-  const output = [...array];
-  const element = output.splice(index, 1)[0];
-  output.splice(position, 0, element);
-  return output;
+  // Clone of the original array using spread syntax (...)
+  const output = [...array]
+  // Remove the element of the array using splice() and store it on the variable 'element'
+  const element = output.splice(index, 1)[0]
+  // Put the element on the new position
+  output.splice(position, 0, element)
+  return output
 }
